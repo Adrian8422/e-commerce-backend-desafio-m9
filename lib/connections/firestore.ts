f74f7e5b191bfafc407a8330f269cc10681c3852
Adrian Villegas <manbassman1996@gmail.com>
2022-10-18 21:19:42 -0300

    update-firestore

--- lib/connections/firestore.ts
+++ lib/connections/firestore.ts
@@ -2,14 +2,14 @@ import admin from "firebase-admin";
 
 var serviceAccount = JSON.parse(process.env.FIREBASE_CONNECTION);
 console.log(serviceAccount);
- if (admin.app.length == 1) {
-   console.log("todook");
- } else {
+//  if (admin.app.length == 1) {
+//    console.log("todook");
+//  } else {
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
-   console.log("data admin", admin.app.length);
- }
+//    console.log("data admin", admin.app.length);
+//  }
 
 const firestore = admin.firestore();
 export { firestore };
