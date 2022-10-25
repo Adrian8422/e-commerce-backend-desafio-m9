82a7f5ffedfe01ad85cc030a9c6fe91a05a904da
Adrian Villegas <manbassman1996@gmail.com>
2022-10-25 17:26:39 -0300

    probando-stock

--- controllers/cart.ts
+++ controllers/cart.ts
@@ -3,7 +3,7 @@ import { Cart } from "models/cart";
 import { Order } from "models/orders";
 import { deleteByIdProduct, getArrayProductsIdAlgolia, getProductIdAlgolia } from "./products";
 
-export async function addProductInCart(idsProducts,userId,dataBody){
+export async function addProductInCart(idsProducts,userId){
   const products = await getArrayProductsIdAlgolia(idsProducts)
 
   if(!products){
@@ -17,55 +17,50 @@ export async function addProductInCart(idsProducts,userId,dataBody){
         categories:prod["categories"],
         description:prod["description"],
         price:prod["price"],
+        stock:prod["stock"],
         productId: prod["objectID"],
         userId: userId,
         createdAt: new Date(),
-        aditional_info: {
-          ...dataBody,
-        },
       })
           
-      
-      const order = await Order.createOrder({
-        ownerId:products.results[0]["ownerId"],
-        productId: products.results.map((prod)=> prod.objectID),
-        userId: userId,
-       status: "pending",
-       createdAt: new Date(),
-       aditional_info: {
-        ...dataBody,
-      },
-      
     })
     
-    const createPreferenceMp = await createPreference({
-      external_reference: order.id,
-      
-      items: 
-      // products.results.map((producto)=>(
-        {     
-             title: prod["title"],
-             description: prod["description"],
+    const order = await Order.createOrder({
+       ownerId:products.results[0]["ownerId"],
+       productId: products.results.map((prod)=> prod.objectID),
+       userId: userId,
+       status: "pending",
+       createdAt: new Date(),
+       
+     })
+
+     const createPreferenceMp = await createPreference({
+       external_reference: order.id,
+
+       items: 
+         products.results.map((producto)=>(
+           {     
+             title: producto["title"],
+             description: producto["description"],
              picture_url: "http://www.myapp.com/myimage.jpg",
-             quantity: prod["aditional_info"].quantity,
+             quantity: 1,
              currency_id: "$",
-             unit_price: prod["price"],
-            }
-            // )
-            
-            // )
-            ,
-            back_urls: {
-              success: "https://portfolio-6357a.web.app/",
-              pending: "https://portfolio-6357a.web.app/",
-            },
-            notification_url:
-            "https://e-commerce-backend-desafio-m9.vercel.app/api/webhooks/mercadopago",
-            //  "https://webhook.site/15eead9d-9d4c-4d53-8dc9-86ad7dba0dd4"
-          });
-          console.log("preference",createPreferenceMp.init_point)
-          return {url:createPreferenceMp.init_point}
-        })
+             unit_price: producto["price"],
+           }
+           )
+         
+       )
+       ,
+       back_urls: {
+         success: "https://portfolio-6357a.web.app/",
+         pending: "https://portfolio-6357a.web.app/",
+       },
+       notification_url:
+         "https://e-commerce-backend-desafio-m9.vercel.app/api/webhooks/mercadopago",
+        //  "https://webhook.site/15eead9d-9d4c-4d53-8dc9-86ad7dba0dd4"
+        });
+        console.log("preference",createPreferenceMp.init_point)
+return {url:createPreferenceMp.init_point}
   
   }
   export async function quitProductCart(idProduct){
--- pages/api/users/me/cart/index.ts
+++ pages/api/users/me/cart/index.ts
@@ -6,9 +6,8 @@ import { addProductInCart, quitAllProductsCart, quitProductCart } from "controll
 
 async function postHandler(req: NextApiRequest, res: NextApiResponse, token) {
   const {id} = req.query
-  const dataBody = req.body
   const userId = token.userId
-  const response = await addProductInCart(id,userId,dataBody)
+  const response = await addProductInCart(id,userId)
   console.log(response)
   res.send(response)
 }
