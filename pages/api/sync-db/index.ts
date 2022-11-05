ef08b11e84fb28bbb08035cad7899c38a3815f0a
Adrian Villegas <manbassman1996@gmail.com>
2022-11-05 00:18:18 -0300

    changes-sync-db

--- pages/api/sync-db/index.ts
+++ pages/api/sync-db/index.ts
@@ -5,32 +5,46 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 
 
-export default function (req: NextApiRequest, res: NextApiResponse) {
-  try {
-     airtableBase("Table 1")
-        .select({})
-        .eachPage(
-           async function (records, fetchNextPage) {
-              const results = records.map((record) => {
-                 return {
-                    objectID: record.id,
-                    ...record.fields,
-                 };
-              });
+export default  function (req: NextApiRequest, res: NextApiResponse) {
+ try {
+  
 
-              await productIndex.saveObjects(results);
-              fetchNextPage();
-           },
+   airtableBase("Table 1")
+    .select({
+      // Selecting the first 3 records in Grid view:
+      pageSize: 10,
+    })
+    .eachPage(
+     async  function (records, fetchNextPage) {
+        // This function (`page`) will get called for each page of records.
+// chequear ahora sin el nombre de la funcion 
+        const obj =  records.map(function (record) {
+          return {
+            objectID: record.id,
+            ownerId:record["ownerId"],
+            ...record.fields,
+          };
+        });
+      
+         await productIndex.saveObjects(obj)
+        
 
-           function done(err) {
-              if (err) {
-                 console.error(err);
-                 return;
-              }
-              res.status(200).send("Done");
-           }
-        );
-  } catch (err) {
-     res.status(500).send({ message: err });
+        // To fetch the next page of records, call `fetchNextPage`.
+        // If there are more records, `page` will get called again.
+        // If there are no more records, `done` will get called.
+        fetchNextPage();
+      },
+      function done(err) {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        res.status(200).send("todo ok")
+      }
+    )
+  } catch (error) {
+    res.status(404).send({message:error})
+  
   }
+  
 }
