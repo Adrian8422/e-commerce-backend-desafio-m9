c67a7dd16039998ed022bc850075ad20555ec923
Adrian Villegas <manbassman1996@gmail.com>
2024-05-15 18:31:17 -0300

    update airtable connection

--- lib/connections/airtable.ts
+++ lib/connections/airtable.ts
@@ -1,4 +1,8 @@
 import Airtable from "airtable";
-export const airtableBase = new Airtable({
+Airtable.configure({
+  endpointUrl: "https://api.airtable.com",
   apiKey: process.env.API_KEY_AIRTABLE,
-}).base(process.env.ID_BASE_APP_AIRTABLE);
+});
+export const airtableBase = Airtable.base(process.env.ID_BASE_APP_AIRTABLE);
+
+console.log("aitabel baseSS", airtableBase);
